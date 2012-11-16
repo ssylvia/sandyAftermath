6069e2a3ebf365ea559cc34f700f4f961f36baf3
Stephen Sylvia <ssylvia@esri.com>
2012-11-16 10:15:32 -0500

    change extent and rearrange sources

--- javascript/layout.js
+++ javascript/layout.js
@@ -2,32 +2,29 @@ var overButton = false;
 var mouseDown = 0, mouseOverA = 0;
 
 var mapExtents = [
-	new esri.geometry.Extent(6215448.627786052,5372806.419964143,6998163.797426219,5855276.942500153, new esri.SpatialReference({ wkid:102100 })), 
-	new esri.geometry.Extent(4107771.1245415304,-372672.42632336804,4205610.520746425,-312363.6110064453, new esri.SpatialReference({ wkid:102100 })),
-	 new esri.geometry.Extent(-16467714.02011686,8596563.575002903,-16272035.227706732,8717181.205636958, new esri.SpatialReference({ wkid:102100 })), 
-	 new esri.geometry.Extent(-9739387.336995596,1450626.1865470726,-9690467.638893148,1480780.594205534, new esri.SpatialReference({ wkid:102100 })), 
-	 new esri.geometry.Extent(12617273.64987246,406090.8799947877,12641733.498923682,421168.0838240184, new esri.SpatialReference({ wkid:102100 })), 
-	 new esri.geometry.Extent(-7154531.207096819,-2074239.9741702052,-6763173.622276904,-1833004.7129023047, new esri.SpatialReference({ wkid:102100 })), 
-	 new esri.geometry.Extent(14473807.224474363,3870252.068861484,14498267.073525585,3885329.272690715, new esri.SpatialReference({ wkid:102100 })), 
-	 new esri.geometry.Extent(-12524848.085687213,7726586.100995238,-12329169.293277085,7847203.7316292925, new esri.SpatialReference({ wkid:102100 })), 
-	 new esri.geometry.Extent(-9081373.933097947,4572973.322950388,-9056914.084046725,4588050.526779619, new esri.SpatialReference({ wkid:102100 })), 
-	 new esri.geometry.Extent(12652271.879276711,2629625.351356609,12676731.728327934,2644702.5551858395, new esri.SpatialReference({ wkid:102100 })), 
-	 new esri.geometry.Extent(-12954937.258830944,3994310.2546197944,-12942707.334305333,4001848.85653441, new esri.SpatialReference({ wkid:102100 })), 
-	 new esri.geometry.Extent(-8632312.505170723,4723087.762920504,-8620082.580645112,4730626.364835119, new esri.SpatialReference({ wkid:102100 }))];
+     //xmin,ymin,xmax,ymax
+	 new esri.geometry.Extent(-8245941.023663707,4856419.421644134,-8245176.65338075,4856946.717706518, new esri.SpatialReference({ wkid:102100 })),
+	 new esri.geometry.Extent(-8288521.179588634,4771025.15913448,-8282406.217325659,4775243.527633079, new esri.SpatialReference({ wkid:102100 })),
+	 new esri.geometry.Extent(-8279334.404251365,4779509.669274361,-8276276.9231198765,4781618.853523661, new esri.SpatialReference({ wkid:102100 })),
+	 new esri.geometry.Extent(-8242112.051567996,4879866.546757705,-8241347.681285039,4880393.842820088, new esri.SpatialReference({ wkid:102100 })),
+	 new esri.geometry.Extent(-8301680.681755428,4762363.988156241,-8300151.941189854,4763418.580280774, new esri.SpatialReference({ wkid:102100 })),
+	 new esri.geometry.Extent(-8367466.6878749095,4605663.302870606,-8365937.947309336,4606717.894995139, new esri.SpatialReference({ wkid:102100 })),
+	 new esri.geometry.Extent(-8233583.996573069,4973812.728808983,-8232819.626290112,4974340.024871367, new esri.SpatialReference({ wkid:102100 })),
+	 new esri.geometry.Extent(-8101197.452221993,5006940.417439494,-8100433.081939036,5007467.713501878, new esri.SpatialReference({ wkid:102100 })),
+	 new esri.geometry.Extent(-8244225.966525398,4871158.819459764,-8242697.225959824,4872213.411584297, new esri.SpatialReference({ wkid:102100 })),
+	 new esri.geometry.Extent(-8228899.14933946,4947022.978238763,-8228134.7790565025,4947550.274301147, new esri.SpatialReference({ wkid:102100 }))];
 
 var extentTitles = [
-	"Aral Sea, Uzbekistan and Kazakhstan", 
-	"Mount Kilimanjaro, Tanzania", 
-	"Columbia Glacier, Alaska, United States", 
-	"Gulf of Fonseca, Honduras", 
-	"Coastal Malaysia", 
-	"Santa Cruz, Bolivia", 
-	"Isahaya Bay, Japan", 
-	"Athabaska oil sands, Alberta, Canada", 
-	"Mountaintop Removal, West Virginia, United States",
-	"Dongguan, China", 
-	"Palm Springs, California, United States", 
-	"Loudoun County, Virginia, United States"
+	"Seaside, NJ",
+	"Atlantic City, NJ",
+	"Brigantine, NJ",
+	"Point Pleasant Beach, NJ",
+	"Ocean City, NJ",
+	"Assateague, MD",
+	"Queens, NY",
+	"Beachway on Long Island",
+	"Mantoloking, NJ",
+	"Breezy Point Fire location, NY"
 	];
 
 var extentText = [
@@ -40,9 +37,7 @@ var extentText = [
 	"An effort to reclaim tidal flats in this area of Japan’s southern island of Kyushu began a year prior to the 1990 image at left. Although a substantial portion of the area behind the seawall (visible as a diagonal gray line) has been fully converted to agriculture, Japanese citizens groups continue to protest the destruction of wetlands and other ecological damage caused by the project.",
 	"Canada’s oil reserves are second only to Saudi Arabia’s. Most of these reserves are in the vast oil sands deposits of Alberta. Extracting the heavy oil, which is mixed with sand, minerals, and water, is expensive both economically and environmentally. As oil prices have increased, extraction has become more economically viable, a trend that is evident in these images.",
 	"America’s ever-increasing demand for energy is met in large part by coal, much of which is extracted from the remote mountains of West Virginia. Mountaintop removal involves stripping forests and overburden from mountaintops to remove the coal. Even after restoration, landscapes are significantly altered.",
-	"Like its neighboring cities of Guangzhou and Shenzhen, Dongguan has grown explosively as rural Chinese have moved to urban centers seeking employment in factories. In 2008, migrants from other parts of China outnumbered Dongguan’s local residents five to two. The images reflect that change; note in the more recent image the new circumferential highway and areas of bare ground at construction sites.",
-	"The desert sunshine of Palm Springs has long been a draw for tourists and retirees. Suburbs and retirement communities continue to expand southeastward toward the Coachella Valley; the city now boasts more than 125 golf courses.",
-	"One of the fastest-growing counties in the U.S., northern Virginia’s Loudoun County increased in population by 84% between 2000 and 2010. In this area near Dulles Airport, rural fields (pink and green) and woodlots (dark green) visible in 1990 have been largely replaced by subdivisions and light industry.<br /><br /><p style='font-family:Arial, Helvetica, sans-serif; font-size:10px'><strong>Sources:</strong> Left, <a style='color:#333; text-decoration:underline; font-weight:normal;' onMouseOver='overA()' onMouseOut='outA()' href='http://www.arcgis.com/home/item.html?id=1edfe16c7aff4f5b9bf10c88a97ec30b' target='_blank'>1990 Landsat imagery</a>; Right, Esri's <a style='color:#333; text-decoration:underline; font-weight:normal;' onMouseOver='overA()' onMouseOut='outA()' href='http://www.arcgis.com/home/item.html?id=10df2279f9684e4a9f6a7f08febac2a9' target='_blank'>World Imagery</a> basemap</p>"];
+	"One of the fastest-growing counties in the U.S., northern Virginia’s Loudoun County increased in population by 84% between 2000 and 2010. In this area near Dulles Airport, rural fields (pink and green) and woodlots (dark green) visible in 1990 have been largely replaced by subdivisions and light industry.<br /><br /><p style='font-family:Arial, Helvetica, sans-serif; font-size:10px'><strong>Sources:</strong> Left, Esri's <a style='color:#333; text-decoration:underline; font-weight:normal;' onMouseOver='overA()' onMouseOut='outA()' href='http://www.arcgis.com/home/item.html?id=10df2279f9684e4a9f6a7f08febac2a9' target='_blank'>World Imagery</a> basemap; Right, <a style='color:#333; text-decoration:underline; font-weight:normal;' onMouseOver='overA()' onMouseOut='outA()' href='http://www.arcgis.com/home/item.html?id=1edfe16c7aff4f5b9bf10c88a97ec30b' target='_blank'>1990 Landsat imagery</a>.</p>"];
 
 var mapIndex = 0;
 
